49407a262ddbd7d824396f790a47df0ac1f2cc09
Susan Imrie <simrie@akamai.com>
2018-12-16 11:07:14 -0500

    Angular calling Express routes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const opn = require('opn');
@@ -17,12 +18,33 @@ const url = 'http://localhost:' + port;
 app.use(express.static(__dirname + '/node_modules'));
 app.use(express.static(__dirname + '/displayPage'));
 app.use(express.static(__dirname + '/operations'));
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-app.get('/', function(req, res, next) {
+
+app.get('/', function(req, res) {
     res.sendFile(__dirname + '/displayPage/index.html');
 });
 
+app.get('/clear', function(req, res) {
+    console.log('clear req ', req);
+    res.send('cleared');
+});
 
+app.post('/index', function(req, res) {
+    console.log('index req ', req.body.urlIP);
+    const obj = {};
+    obj['request'] = 'indexed';
+    res.json(obj);
+});
+
+app.post('/search', function(req, res) {
+    console.log('search req ', req.body.words);
+    const obj = {};
+    obj['request'] = 'searched';
+    res.json(obj);
+    res.end();
+});
 
 // Tell the server what port to run on
 server.listen(port);
--- operations/angularCtrl.js
+++ operations/angularCtrl.js
@@ -11,17 +11,44 @@ const clear = require('./clear.js');
 const angtest = require('./angtest.js');
 */
 
+
 //const angularApp = angular.module('myApp').controller('myCtrl', function($scope) {
-const angularCtrl = ($scope) => {
+const angularCtrl = ($scope, $http) => {
+        //$http callback functions
         $scope.results = angtest.nimportQuoi();
+        const success = (response) => {
+            console.log('success response', response);
+            $scope.results = response.data;;
+        };
+        const err = (err) => {
+            console.log('err ', err);
+            $scope.results = err;
+        };
         $scope.clear = function() {
-            $scope.results = clear.doClear();
+            //$scope.results = clear.doClear();
+            $http.get('/clear').then(success, err);
         };
         $scope.index = function() {
-            $scope.results = indexing.doIndex($scope.input);
+            //$scope.results = indexing.doIndex($scope.input);
+            const urlIP = $scope.input;
+            $http({
+                url: '/index',
+                method: 'POST',
+                data: { 'urlIP' : urlIP },
+                headers: {'Content-Type': 'application/json'}
+            }).then(success, err);
         };
         $scope.search = function() {
-            $scope.results = searching.doSearch($scope.input);
+            //$scope.results = searching.doSearch($scope.input);
+            const searchParams = $scope.input;
+            //application/json
+            //application/x-www-form-urlencoded
+            $http({
+                url: '/search',
+                method: 'POST',
+                data: { 'words' : searchParams },
+                headers: {'Content-Type': 'application/json'}
+            }).then(success, err);
         };
         $scope.myFunc = function() {
             $scope.results = angtest.autrechose($scope.input);
